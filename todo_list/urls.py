881f72b08ff60f8a1e8c740acfb0994a2666201d
shivraj1192 <2020bpr012@sggs.ac.in>
2023-09-08 13:15:57 +0530

    commit changes

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('/var/task/admin/', admin.site.urls),  
+    path('var/task/admin/', admin.site.urls),  
     path('', include('todo_app.urls')),
 ]
 
