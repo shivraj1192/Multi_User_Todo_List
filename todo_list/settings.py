c54204977831040346d8552b4b63236c382b04c3
shivraj1192 <2020bpr012@sggs.ac.in>
2023-09-08 14:11:50 +0530

    commit changes

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xdcbs=gnl@x$@!w0##sjmwq+$0_ob55w(mmw_)6$m1tc=g61s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
