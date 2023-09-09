61502f412b1f8c23e3c3015b29938db6ad86da9a
shivraj1192 <2020bpr012@sggs.ac.in>
2023-09-09 10:36:42 +0530

    admin changes

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
+
 from django.urls import path, include
 from todo_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name='admin'), 
+    
     path('', views.home , name='home'),
 
     path('todo/', views.todo , name='todo'),
