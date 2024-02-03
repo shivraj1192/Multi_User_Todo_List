4f43ba30bd2c42ae16eafa7887932d07174d92c2
shivraj1192 <2020bpr012@sggs.ac.in>
2024-02-03 11:04:00 +0530

    initial changes

--- todo_app/views.py
+++ todo_app/views.py
@@ -11,65 +11,63 @@ def home(request):
 
 
 def todo(request):
-    # if request.user.is_authenticated:
-    #     user = request.user
-    #     todos = Todo.objects.filter(user = user).order_by('priority')
-    #     context = {
-    #         'todos' : todos
-    #     }
-    #     return render(request, "todo.html" , context = context)
-    # else:
-    #     return render(request,'LogIn.html')
-    
-    return render(request, "todo.html")
+    if request.user.is_authenticated:
+        user = request.user
+        todos = Todo.objects.filter(user = user).order_by('priority')
+        context = {
+            'todos' : todos
+        }
+        return render(request, "todo.html" , context = context)
+    else:
+        return render(request,'LogIn.html')
     
 
 
 
 def SignUp(request):
-    # if request.method=="POST":
-    #     get_name= request.POST.get('name')
-    #     get_email= request.POST.get('email')
-    #     get_password= request.POST.get('pass1')
-    #     get_confirm_password= request.POST.get('pass2')
-    #     if get_password == get_confirm_password:
-    #         pass
-    #     else:
-    #         messages.info(request,'password is not matching')
-    #         return redirect("/SignUp/")
+    if request.method=="POST":
+        get_name= request.POST.get('name')
+        get_email= request.POST.get('email')
+        get_password= request.POST.get('pass1')
+        get_confirm_password= request.POST.get('pass2')
+        if get_password == get_confirm_password:
+            pass
+        else:
+            messages.info(request,'password is not matching')
+            return redirect("/SignUp/")
         
-    #     try:
-    #         if User.objects.get(username=get_name):
-    #             messages.warning(request,"This username is already in use!")
-    #             return redirect("/SignUp/")
-    #         if User.objects.get(email=get_email):
-    #             messages.warning(request,"This email is already in use!")
-    #             return redirect("/SignUp/")
-    #     except Exception as identifier:
-    #         pass
+        try:
+            if User.objects.get(username=get_name):
+                messages.warning(request,"This username is already in use!")
+                return redirect("/SignUp/")
+            if User.objects.get(email=get_email):
+                messages.warning(request,"This email is already in use!")
+                return redirect("/SignUp/")
+        except Exception as identifier:
+            pass
 
-    #     myuser=User.objects.create_user(get_name,get_email,get_password)
-    #     myuser.save()
-    #     messages.success(request,"User is created please Log In!")        
-    #     return redirect("/LogIn/")
+        myuser=User.objects.create_user(get_name,get_email,get_password)
+        myuser.save()
+        messages.success(request,"User is created please Log In!")        
+        return redirect("/LogIn/")
     return render(request,'SignUp.html')
 
 
 
 
 def handleLogin(request):
-    # if request.method=="POST":
-    #     get_name= request.POST.get('name')
-    #     get_password= request.POST.get('pass1')
-    #     myuser= authenticate(username=get_name,password=get_password)
+    if request.method=="POST":
+        get_name= request.POST.get('name')
+        get_password= request.POST.get('pass1')
+        myuser= authenticate(username=get_name,password=get_password)
 
-    #     if myuser is not None:
-    #         login(request,myuser) 
-    #         messages.success(request,"Successfully LogIn")
-    #         return redirect("/todo/")
-    #     else:
-    #         messages.error(request,"Invalid Credentials")
-    #         return redirect("/LogIn/")
+        if myuser is not None:
+            login(request,myuser) 
+            messages.success(request,"Successfully LogIn")
+            return redirect("/todo/")
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect("/LogIn/")
     return render(request,'LogIn.html')
 
 
@@ -84,32 +82,21 @@ def handleLogout(request):
 
 
 def AddTodo(request):
-    # if request.user.is_authenticated:
-    #     user = request.user
-    #     print(user)
+    if request.user.is_authenticated:
+        user = request.user
+        print(user)
 
-    #     if request.method=="POST":
-    #         get_title= request.POST.get('title')
-    #         get_status= request.POST.get('status')
-    #         get_priority= request.POST.get('priority')
-    #         duedate_start = request.POST.get('startdate')
-    #         duedate_end = request.POST.get('enddate')
-
-    #         mytodo = Todo(title=get_title, status=get_status, priority=get_priority, user=user , duedate_start = duedate_start, duedate_end = duedate_end)
-    #         mytodo.save()
-    #         print(mytodo)
-    #     return redirect('/todo/')
-    if request.method=="POST":
+        if request.method=="POST":
             get_title= request.POST.get('title')
             get_status= request.POST.get('status')
             get_priority= request.POST.get('priority')
             duedate_start = request.POST.get('startdate')
             duedate_end = request.POST.get('enddate')
 
-            mytodo = Todo(title=get_title, status=get_status, priority=get_priority, duedate_start = duedate_start, duedate_end = duedate_end)
+            mytodo = Todo(title=get_title, status=get_status, priority=get_priority, user=user , duedate_start = duedate_start, duedate_end = duedate_end)
             mytodo.save()
             print(mytodo)
-    return redirect('/todo/')
+        return redirect('/todo/')
     
 
 
